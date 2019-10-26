031cb8423ac00415eb195ac3194848cbc7b79685
Olzhas Aitkaliyev <mysyqbai@gmail.com>
2019-10-26 15:40:23 +0200

    Fix замечаний

--- dao.py
+++ dao.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Table, MetaData, Column, Integer, String, DateTime
 
+
 metadata = MetaData()
 catalog = Table('catalog', metadata,
                 Column('id', Integer, primary_key=True),
@@ -11,17 +12,17 @@ catalog = Table('catalog', metadata,
 
 
 def conn():
-    db_string = "postgres://voexynoieveheo:b8e1d89b13851ba61e7cc7a4567d30d3613a55b19610b670a749690f4322dc76@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/dck1ojnl5uc1sb"
-    db = create_engine(db_string)
+    db_address = "postgres://voexynoieveheo:b8e1d89b13851ba61e7cc7a4567d30d3613a55b19610b670a749690f4322dc76@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/dck1ojnl5uc1sb"
+    db = create_engine(db_address)
     return db
 
 
-def add_catalog(gtin, quantity, shop_id):
-    st = catalog.insert().values(gtin=gtin, quantity=quantity, shop_id=shop_id)
-    conn().execute(st)
+def add_catalog(gtin: str, quantity: int, shop_id: str):
+    statement = catalog.insert().values(gtin=gtin, quantity=quantity, shop_id=shop_id)
+    conn().execute(statement)
 
 
 def get_all():
-    st = catalog.select()
-    result = conn().execute(st)
+    statement = catalog.select()
+    result = conn().execute(statement)
     return [dict(row) for row in result]
