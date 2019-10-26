688a39a1ba540a76244dcab69d9430377940af70
Olzhas Aitkaliyev <mysyqbai@gmail.com>
2019-10-26 17:30:10 +0200

    Вынес настройки в конфиг

---
+++ config.py
@@ -0,0 +1,7 @@
+db = {
+    'user': 'voexynoieveheo',
+    'password': 'b8e1d89b13851ba61e7cc7a4567d30d3613a55b19610b670a749690f4322dc76',
+    'host': 'ec2-54-246-92-116.eu-west-1.compute.amazonaws.com',
+    'port': 5432,
+    'database': 'dck1ojnl5uc1sb'
+}
