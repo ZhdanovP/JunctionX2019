16ccbce1f7799c061d130d48d4e92c196e89cdc3
Olzhas Aitkaliyev <mysyqbai@gmail.com>
2019-10-26 16:20:22 +0200

    fix run

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask import request
 import dao
+import os
 
 app = Flask(__name__)
 
@@ -29,4 +30,5 @@ def get_all():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
